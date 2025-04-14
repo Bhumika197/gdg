fd3c43aae5280504ad275d52971b0fc072e8e4c5
Bhumika197 <chintabhumika07@gmail.com>
2025-04-14 13:07:22 +0530

    new files added

---
+++ JFP.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main(){
+    int a = 5; 
+    ++a; 
+    printf("%d\n", a);
+     a = 5; 
+    printf("%d\n", a++);
+    a = 5; 
+    printf("%d\n", --a);
+     a = 5; 
+    printf("%d\n", a--);
+    int x = 5 +6 - 8*10/5; 
+    printf("%d\n", x);
+    int p =8; 
+    int q =5; 
+    p =p +q; 
+    printf("%d\n", p);
+    printf("%d", q);
+    return 0;
+}
